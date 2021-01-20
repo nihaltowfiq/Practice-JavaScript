c5c81ba75b0f42b0e621cb049da27f8bd986f3d3
nihaltowfiq <nihaltowfiq@gmail.com>
2021-01-20 18:28:42 +0600

    character set, quantifier, grouping added

--- 007. regex-practice/regex-2.js
+++ 007. regex-practice/regex-2.js
@@ -14,7 +14,34 @@ regex = /h*llo/; // * = 0 or more character;
 regex = /he?llo/; // ? = opitonal charater;
 regex = /hello\?/; // \ = escaping;
 
-str = "hello? world";
+// Character Set using Bracket [];
+regex = /h[ea]llo/; // [] = must be one of them inside bracket;
+regex = /h[^ea]llo/; // [^] = anything except those inside bracket;
+regex = /^[ha]ello/; // ^[] = must start with those inside bracket;
+regex = /[a-h]ello/; // [-] = must be start between them(-) inside bracket;
+regex = /[A-Z]ello/; // first letter must be uppercase;
+regex = /[A-Za-z]ello/; // first letter upper or lower case;
+regex = /[0-9]ello/; // first letter must be number digit;
+regex = /^[0-9]ello/; // must start with number digit;
+regex = /[^0-9]ello/; // no digit;
+regex = /[0-9][0-9]ello/; // two digit;
+regex = /[0-9][0-9][0-9][0-9][0-9]/; //five digit;
+
+// Braces {} - Quantifier;
+regex = /hel{2}o/; // must occur exacly two times;
+regex = /hel{3,5}o/; // range;
+regex = /hel{2,}o/; // at least 2 times;
+
+// Parentheses () - Grouping;
+regex = /([0-9]{5})/; // /[0-9][0-9][0-9][0-9][0-9]/; //five digit; => same!!
+regex = /[(0-9)x]{3}/;
+
+// Let's check Bangladesh Phone Number;
+regex = /^01[^0-2][0-9]{8}$/;
+regex = /^\+8801[^0-2][0-9]{8}$/;
+
+str = "01788888888";
+str = "+8801788888888";
 
 console.log(regex.exec(str));
 
